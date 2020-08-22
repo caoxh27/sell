5b4a74c35a207836887cc18ea23c603a8fb0052b
caoxh27 <caoxh27@qq.com>
2020-08-22 13:05:15 +0800

    ProductService

--- src/main/java/com/imooc/sell/service/ProductService.java
+++ src/main/java/com/imooc/sell/service/ProductService.java
@@ -1,4 +1,35 @@
 package com.imooc.sell.service;
 
+import com.imooc.sell.dataobject.ProductInfo;
+
+import java.util.List;
+
 public interface ProductService {
+    ProductInfo findById(String productId);
+
+    /**
+     * 客户端查询所有在架商品列表
+     * @return
+     */
+    List<ProductInfo> findUpAll();
+
+    /**
+     * 服务端查询所有商品列表
+     * @return
+     */
+    List<ProductInfo> findAll();
+
+    /**
+     * 保存商品、
+     * @param productInfo
+     * @return
+     */
+    ProductInfo save(ProductInfo productInfo);
+
+
+
+    //加库存
+
+    //减库存
+
 }
