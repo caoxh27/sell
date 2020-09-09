45687b7a4b110fa2554ffcc185975ca4dfd4ca3a
caoxh27 <caoxh27@qq.com>
2020-09-09 08:28:49 +0800

    BuyerOrderController p20200908

--- src/main/java/com/imooc/sell/controller/BuyerOrderController.java
+++ src/main/java/com/imooc/sell/controller/BuyerOrderController.java
@@ -1,13 +1,22 @@
 package com.imooc.sell.controller;
 
 import com.imooc.sell.VO.ResultVO;
+import com.imooc.sell.converter.OrderForm2OrderDTOConverter;
+import com.imooc.sell.dto.OrderDTO;
+import com.imooc.sell.enums.ResultEnum;
+import com.imooc.sell.exception.SellException;
 import com.imooc.sell.form.OrderForm;
+import com.imooc.sell.service.OrderService;
+import com.imooc.sell.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,14 +24,36 @@ import java.util.Map;
 @Slf4j
 public class BuyerOrderController {
 
+    @Autowired
+    OrderService orderService;
+
     // 创建订单
     // 根据api获知方法返回 和 参数；参数需要做表单验证
     public ResultVO<Map<String,String>> create(@Valid OrderForm orderForm, BindingResult bindingResult){
 
-        return null;
+        if(bindingResult == null){
+            log.error("【创建订单】参数不正确，orderForm={}",orderForm);
+            throw new SellException(ResultEnum.PARAM_ERROR.getCode(),
+                    bindingResult.getFieldError().getDefaultMessage());
+
+        }
+
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
+            log.error("【创建订单】购物车不能为空");
+            throw new SellException(ResultEnum.CART_EMPTY);
+        }
+
+        OrderDTO createResult = orderService.create(orderDTO);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", createResult.getOrderId());
+
+        return ResultVOUtil.success(map);
     }
 
     // 订单列表
+    //TODO 1/4
 
     //订单详情
 
---
+++ src/main/java/com/imooc/sell/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,41 @@
+package com.imooc.sell.converter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.imooc.sell.dataobject.OrderDetail;
+import com.imooc.sell.dto.OrderDTO;
+import com.imooc.sell.enums.ResultEnum;
+import com.imooc.sell.exception.SellException;
+import com.imooc.sell.form.OrderForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class OrderForm2OrderDTOConverter {
+    public static OrderDTO convert(OrderForm orderForm){
+        Gson gson = new Gson();
+        OrderDTO orderDTO = new OrderDTO();
+
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerPhone(orderForm.getPhone());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setBuyerOpenid(orderForm.getOpenid());
+        //BeanUtils.copyProperties();   BuyerName ,Name 不一致，不能使用属性拷贝工具
+
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        try {
+            orderDetailList = gson.fromJson(orderForm.getItems(),
+                    new TypeToken<List<OrderDetail>>() {
+                    }.getType());
+        } catch (Exception e) {
+            log.error("【对象转换】错误, string={}", orderForm.getItems());
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+}
